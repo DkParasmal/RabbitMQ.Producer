9adb773b63bcb247e6cfbb7316c1ae438c02050c
Dilip kumar <dilip.kumar@doxc.in>
2021-06-02 01:01:09 +0530

    Add project files.

---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var message = new { Name = "DK", Message = "Hello i am new to queue" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            channel.BasicPublish("", "demo-queue", null, body);
+            var consumerCount= channel.ConsumerCount("demo-queue");
+
+            Console.WriteLine("Hello World!"+ consumerCount);
+        }
+    }
+
+
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public interface IDependency
+    {
+        int Value { get; }
+    }
+
+    internal class DependencyClass : IDependency
+    {
+        public int Value => 1;
+    }
+
+    public class MyAwesomeTests
+    {
+        private readonly IDependency _d;
+
+        public MyAwesomeTests(IDependency d) => _d = d;
+
+        [Fact]
+        public void AssertThatWeDoStuff()
+        {
+            Assert.Equal(1, _d.Value);
+        }
+    }
+}
